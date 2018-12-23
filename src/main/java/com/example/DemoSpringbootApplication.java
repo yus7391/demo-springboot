965f5293402fec4c7e20f8976fb4ffdbbee0ab77
yusong <yus7391@163.com>
2018-12-23 21:45:39 +0800

    代码优化

--- src/main/java/com/example/DemoSpringbootApplication.java
+++ src/main/java/com/example/DemoSpringbootApplication.java
@@ -5,17 +5,19 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yusong
+ */
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class DemoSpringbootApplication {
 
-	public static void main(String[] args) {
-		System.out.println("main begin ....");
-//		SpringApplication.run(DemoSpringbootApplication.class, args);
-		SpringApplication app = new SpringApplication(DemoSpringbootApplication.class);
-		//去掉 banner
-		app.setBannerMode(Banner.Mode.OFF);
-		app.run(args);
-		System.out.println("main end ....");
-	}
+    public static void main(String[] args) {
+        System.out.println("main begin ....");
+        SpringApplication app = new SpringApplication(DemoSpringbootApplication.class);
+        //去掉 banner
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+        System.out.println("main end ....");
+    }
 }
